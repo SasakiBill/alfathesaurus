016fe9e45e7e8a4e8e0657c6325f2430f044db50
João Pedro Kelniar <67113082+SasakiBill@users.noreply.github.com>
2021-09-02 00:43:14 -0300

    Initial commit

---
+++ public/resultSearch.php
@@ -0,0 +1,42 @@
+<?php
+    include 'header.php';
+    
+?>
+<div class="topnav">
+        <img src="../view/img/Capturar-removebg-preview.png">
+        <a href="#">Sobre</a>
+        <a href="#">Contribua!</a>
+        <a href="#">Tutoriais</a>
+        <a href="#">Tesauros da Computação</a>
+        <a href="../public/index.php">Página Inicial</a>  
+    <div class="search-container">
+    <form action="search.php" method="POST">
+      <input type="text" placeholder="Busque aqui pelo tesauro..." name="search">
+      <button type="submit" name="submit-search"><i class="fa fa-search">Go</i></button>
+    </form>
+  </div>
+</div>
+<h2>Trodoon - DICIONÁRIO GRATUÍTO DE SINÔNIMOS DA COMPUTAÇÃO</h2>
+
+
+<div class ="cconceito-container">
+    <h4 style="font-size: 36px">Página detalhada do Conceito</h4>
+    <div class="conceito-container">
+    <?php
+    $id = $_GET['threadid'];
+    $sql = "SELECT * FROM `conceito` WHERE idConceito=$id"; 
+    $result = mysqli_query($conn, $sql);
+ 
+    while($row = mysqli_fetch_assoc($result)){
+        echo "ID: " . $row['idConceito']. "<br>";
+        echo "<h4>Título do Conceito Preferêncial: </h4> <h5>" . $row['prefLabel'] . "</h5>";
+        echo "<h4>Título do conceito em Inglês: </h4> <h5>" . $row['altLabelA'] . "</h5>";
+        echo "<h4>Definição Acadêmica: </h4> <h5>" . $row['definitionA'] . "</h5>";
+        echo "<h4>Definição Acadêmica em Inglês: </h4> <h5>" . $row['definitionB'] . "</h5>";
+        echo "<h4>Definição da Comunidade: </h4> <h5>" . $row['definitionComun'] . "</h5>";
+        echo "<h4>Conceito do tipo: </h4> <h5>" . $row['tipoConceito'] . ".</h5>";
+        echo "<h4>Pertence ao Tesauro de: </h4> <h5>" . $row['pertenceTesauro'] . ".</h5>";
+    }
+    ?>
+    </div>
+</div>
