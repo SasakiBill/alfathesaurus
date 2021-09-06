1d897d0a172ac7d209c3aac5958f4b1405468cbd
João Pedro Kelniar <67113082+SasakiBill@users.noreply.github.com>
2021-09-06 17:36:01 -0300

    Update search.php

--- public/search.php
+++ public/search.php
@@ -25,7 +25,7 @@
         if (isset($_POST['submit-search'])){
             $search = mysqli_real_escape_string($conn, $_POST['search']);
             $sql = "SELECT * FROM conceito WHERE prefLabel LIKE '%$search%'
-            OR altLabelA LIKE '%$search%' OR definitionA LIKE '%$search%' OR tipoConceito LIKE '%$search%'";
+            OR altLabelA LIKE '%$search%' OR definitionA LIKE '%$search%' OR tipoConceito LIKE '%$search%' OR definitionComun like '%$search$'";
 
             $result = mysqli_query($conn, $sql);
             $queryResult = mysqli_num_rows($result);
